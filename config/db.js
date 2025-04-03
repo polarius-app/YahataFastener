d81e44728e0ba1b89d36f7bb0a910bc3bac1dff7
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2025-04-03 13:56:09 +0700

    coba commit dari mac

--- config/db.js
+++ config/db.js
@@ -1,7 +1,9 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config(); // Memuat variabel lingkungan dari file .env
 
-const sequelize = new Sequelize('yahata', 'root', '', {
-  host: 'localhost',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS || '', {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   dialect: 'mysql',
 });
 
